f06c7c6efe03c4d689ac232c9a44280676ffc0e0
Your Name <you@example.com>
2026-06-30 08:18:28 +0700

    indexing operators

---
+++ indexing.py
@@ -0,0 +1,11 @@
+# indexing = accessing elements of sequence using [] (indexing operator)
+# [start: end : step]
+credit_number = "1234-5678-1234-6789"
+
+#print(credit_number[0:4])
+# print(credit_number[10:14])
+# print(credit_number[5:])
+# print(credit_number[-6])
+print(credit_number[0::3])
+
+
